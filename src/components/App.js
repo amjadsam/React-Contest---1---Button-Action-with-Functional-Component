3ec92bea5640aa1918245fa18f5b89dbc7819a8c
amjadsam <68984249+amjadsam@users.noreply.github.com>
2021-01-16 23:20:38 +0530

    changed

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,17 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [count,setCount]=useState(0);
+  function showPara()
+  {
+    let a=count;
+    a++;
+    setCount(a);
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={showPara}>click</button>
+    {count!==0?<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>:null}
     </div>
   );
 }
